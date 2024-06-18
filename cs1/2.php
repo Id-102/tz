430abada15c4c18b00d4595b8be1147fa013eb42
丨蛋 <99737935+3122251458@users.noreply.github.com>
2024-06-18 23:40:45 +0800

    Update 2.php

--- cs1/2.php
+++ cs1/2.php
@@ -16,7 +16,7 @@
     <script>
         function redirectToSecondPage() {
             setTimeout(() => {
-                window.location.href = 'http://ceshi001.ml.jun04.top/7.php';
+                window.location.href = 'https://3122251458.github.io/tz/7.php';
             }, 800); // 0.8秒后跳转到第二个页面
         }
     </script>
